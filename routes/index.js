2c4df72a0527c2afdbf4610f4489874326868ddb
mckgar <kmgarner23@outlook.com>
2022-10-12 13:58:30 -0600

    Created and tested login route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const chatRoute = require('./chat');
+const userRoute = require('./user');
+const loginRoute = require('./login');
 
 router.get('/', (req, res, next) => {
   res.send("Under construction");
 });
+
+router.use('/user', userRoute);
+
+router.use('/login', loginRoute);
 
 router.use('/chat', chatRoute);
 
